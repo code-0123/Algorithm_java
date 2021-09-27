7641637abc55eba61fc9d1b0b1d6b3c14e6fe454
jaeout <jaeout2@gmail.com>
2021-09-27 17:10:56 +0900

    Do it 4-2 복습

--- src/chap04/IntStackTester.java
+++ src/chap04/IntStackTester.java
@@ -1,57 +1,59 @@
 package chap04;
+
 import java.util.Scanner;
 
 public class IntStackTester {
-	
+
 	public static void main(String[] args) {
-		
-		Scanner stdIn = new Scanner(System.in); 
-		
-		IntStack s = new IntStack(64); 
-		
-		while(true) {
+
+		Scanner stdIn = new Scanner(System.in);
+
+		IntStack s = new IntStack(64); // 최대 64개를 푸시할 수 있는 스택
+
+		while (true) {
 			System.out.println("현재 데이터 수 : " + s.size() + " / " + s.capacity());
-			System.out.print("(1)푸시 (2)팝 (3)피크  (4)덤프 (0)종료 : "); 
-			
+			System.out.print("(1)푸시 (2)팝 (3)피크  (4)덤프 (0)종료 : ");
+
 			int menu = stdIn.nextInt();
-			if(menu == 0) break; 
-			
-			int x; 
-			switch(menu) {
-			case 1: 
+			if (menu == 0)
+				break;
+
+			int x;
+			switch (menu) {
+			case 1:                           // 푸시
 				System.out.print("데이터 : ");
-				x = stdIn.nextInt(); 
+				x = stdIn.nextInt();
 				try {
-					s.push(x); 
-				}catch(IntStack.OverflowIntStackException e) {
+					s.push(x);
+				} catch (IntStack.OverflowIntStackException e) {
 					System.out.println("스택이 가득 찼습니다");
 				}
-				break; 
-			
-			case 2: 
+				break;
+
+			case 2:                          //팝
 				try {
-					x = s.pop(); 
+					x = s.pop();
 					System.out.println("팝한 데이터는 " + x + "입니다.");
-				}catch(IntStack.EmptyIntStackException e) {
+				} catch (IntStack.EmptyIntStackException e) {
 					System.out.println("스택이 비어 있습니다.");
 				}
-				break; 
-				
-			case 3: 
+				break;
+
+			case 3:                        //피크 : 스택의 꼭대기에 있는 데이터를 몰래 엿보는 메서드
 				try {
-					x = s.peek(); 
+					x = s.peek();
 					System.out.println("피크한 데이터는 " + x + "입니다.");
-				}catch(IntStack.EmptyIntStackException e) {
+				} catch (IntStack.EmptyIntStackException e) {
 					System.out.println("스택이 비어 있습니다.");
 				}
-				break; 
-				
-			case 4:
-				s.dump();
-				break; 
+				break;
+
+			case 4:                       //덤프
+				s.dump();                //스택에 쌓여 있는 모든 데이터를 바닥에서 꼭대기 순으로 표시하는 메서드 
+				break;
 			}
 		}
-		
+
 	}
 
 }
