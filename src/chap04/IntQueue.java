263e46d6ab9b619f01a8297c46619e9104dc867e
jaeout <jaeout2@gmail.com>
2021-09-29 22:22:22 +0900

    Do it 4-3[D]

--- src/chap04/IntQueue.java
+++ src/chap04/IntQueue.java
@@ -57,8 +57,59 @@ public class IntQueue {
 		return x; 
 	}
 	
+	//큐에서 데이터를 피크(프런트 데이터를 들여다봄) 
+	public int peek() throws EmptyIntQueueException{
+		if(num <= 0)
+			throw new EmptyIntQueueException(); 
+		return que[front];
+	}
+	
+	//큐에서 x를 검색하여 인덱스(찾지 못하면 -1)를 반환
+	public int indexOf(int x) {
+		for(int i=0; i<num; i++) {
+		int idx = (i + front) % max;
+		if(que[idx]==x) //검색 성공
+			return idx;
+	}
+	return -1; //검색 실패 
+	}
 	
 	
+	//큐를 비움
+	public void clear() {
+		num = front = rear = 0; 
+	}
+	
+	//큐의 용량을 반환
+	public int capacity() {
+		return max; 
+	}
+	
+	//큐에 쌓여 있는 데이터 수를 반환
+	public int size() {
+		return num;
+	}
+	
+	//큐가 비어 있나요?
+	public boolean isEmpty() {
+		return num <= 0; 
+	}
+	
+	//큐가 가득 찼나요?
+	public boolean isFull() {
+		return num >= max; 
+	}
+	
+	//큐 안의 모든 데이터를 프런트->리어 순으로 출력 
+	public void dump() {
+		if(num <= 0)
+			System.out.println("큐가 비어 있습니다.");
+		else {
+			for (int i=0; i<num; i++)
+				System.out.print(que[(i + front) % max] + " ");
+			System.out.println();
+		}
+	}
 }
 
 
