637c96bd6aa1a63dd71d7fee3dc5b62d88913af2
jaeout <jaeout2@gmail.com>
2021-10-18 22:59:23 +0900

    백준 9085

---
+++ src/baekjoon/N9085.java
@@ -0,0 +1,23 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class N9085 {
+	
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        
+	        int t = sc.nextInt();
+	        
+	        for (int k = 0; k < t; k++) {
+	            int n = sc.nextInt();
+	            int sum = 0;
+	            for (int i = 0; i < n; i++) {
+	                sum += sc.nextInt();
+	            }
+	            System.out.println(sum);
+	        }
+	        
+	        sc.close();
+	    }
+	}
+
